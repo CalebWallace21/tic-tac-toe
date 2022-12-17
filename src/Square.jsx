6cb68635a2127f47da9cfa0ab756b34cda94868e
Caleb Wallace <wallacekid1996@gmail.com>
2022-12-16 17:27:52 -0700

    Finished Project

---
+++ src/Square.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Square = (props) => {
+const handleClick = () => {
+  if (!props.squareValue) {
+  if(props.player) {
+    props.squares.splice(props.index, 1, "X")
+    props.setSquares(props.squares)
+    props.setPlayer(!props.player);
+  }
+  else {
+    props.squares.splice(props.index, 1, "O")
+    props.setSquares(props.squares)
+    props.setPlayer(!props.player);
+    }
+  }
+}
+    return (
+        <div className="square" onClick={handleClick} >
+          {props.squareValue === 0 ? "O" : props.squareValue}
+          {/* {console.log(props.squares, props.player)} */}
+          
+        </div>
+    );
+};
+
+export default Square;
